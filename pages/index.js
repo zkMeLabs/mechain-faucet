98fb36bd31a7c59cd0494b66f0403b1916c2a2ae
Luke <luchenqun@qq.com>
2024-07-16 11:33:55 +0800

    add ethos

--- pages/index.js
+++ pages/index.js
@@ -1,72 +1,56 @@
-import React, { Component } from "react";
-import axios from "axios";
-import dayjs from "dayjs";
-import FingerprintJS from "@fingerprintjs/fingerprintjs";
-import { Layout, Image, message, Button, Col, Row, Card, Input, Spin } from "antd";
-import { FacebookOutlined, GithubOutlined, TwitterOutlined, WeiboOutlined, GooglePlusOutlined } from "@ant-design/icons";
-const { Header, Content, Footer } = Layout;
-import Foot from "../components/foot";
+import React, { Component } from 'react';
+import axios from 'axios';
+import dayjs from 'dayjs';
+import FingerprintJS from '@fingerprintjs/fingerprintjs';
+import { Layout, Image, message, Col, Row, Card, Input, Spin } from 'antd';
+import Foot from '../components/foot';
 
 const sleep = (time) => {
   return new Promise((resolve) => setTimeout(resolve, time));
 };
+
+const showMessageWithLink = (to, hash) => {
+  const link = 'https://scan.ethos.cool/tx/' + hash;
+  const content = (
+    <span>
+      Success send 1 ETHOS to {to}, more detail see
+      <a rel="noreferrer" href={link} target="_blank" style={{ color: 'blue' }}>
+        &nbsp;EthosScan
+      </a>
+    </span>
+  );
+
+  message.success({
+    content,
+    duration: 30,
+  });
+};
 export default class App extends Component {
   state = {
     loading: false,
-    to: "",
-    hash: "",
-    err: "",
+    to: '',
+    hash: '',
+    err: '',
     result: false,
-    inputAddress: "",
+    inputAddress: '',
   };
-  id = "";
+  id = '';
 
   async componentDidMount() {
     try {
       const load = await FingerprintJS.load();
       const result = await load.get();
-      console.log("visitorId", result.visitorId);
+      console.log('visitorId', result.visitorId);
       this.id = result.visitorId.toLowerCase();
     } catch (error) {
-      this.id = dayjs().format("yyyyMMdd");
+      this.id = dayjs().format('yyyyMMdd');
     }
-
-    // window.AWSC.use("nc", function (state, module) {
-    //   // 初始化
-    //   window.nc = module.init({
-    //     // 应用类型标识。它和使用场景标识（scene字段）一起决定了滑动验证的业务场景与后端对应使用的策略模型。您可以在阿里云验证码控制台的配置管理页签找到对应的appkey字段值，请务必正确填写。
-    //     appkey: "CF_APP_1",
-    //     //使用场景标识。它和应用类型标识（appkey字段）一起决定了滑动验证的业务场景与后端对应使用的策略模型。您可以在阿里云验证码控制台的配置管理页签找到对应的scene值，请务必正确填写。
-    //     scene: "register",
-    //     // 声明滑动验证需要渲染的目标ID。
-    //     renderTo: "nc",
-    //     width: "450",
-    //     language: "en",
-    //     test: module.TEST_PASS,
-    //     //前端滑动验证通过时会触发该回调参数。您可以在该回调参数中将会话ID（sessionId）、签名串（sig）、请求唯一标识（token）字段记录下来，随业务请求一同发送至您的服务端调用验签。
-    //     success: function (data) {
-    //       window.console && console.log(data.sessionId)
-    //       window.console && console.log(data.sig)
-    //       window.console && console.log(data.token)
-    //     },
-    //     // 滑动验证失败时触发该回调参数。
-    //     fail: function (failCode) {
-    //       window.console && console.log(failCode)
-    //     },
-    //     // 验证码加载出现异常时触发该回调参数。
-    //     error: function (errorCode) {
-    //       window.console && console.log(errorCode)
-    //     }
-    //   });
-    // })
   }
 
   send = async () => {
-    const { value } = this.inputAddress.state;
-    let to = value;
-
+    let { value: to } = this.inputAddress.input;
     if (!to) {
-      message.error("Please enter an address");
+      message.error('Please enter an address');
       return;
     } else if (!/^(0x)?[0-9a-f]{40}$/i.test(to)) {
       message.error(to + " it's not a valid address");
@@ -74,28 +58,27 @@ export default class App extends Component {
     }
 
     this.setState({ loading: true, result: false });
-    let hash = "";
-    let err = "";
-    await sleep(100);
+    let hash = '';
+    let err = '';
+
     try {
       const reply = await axios.post(`/api/faucet`, { to, id: this.id });
       const { status, statusText, data } = reply;
       if (status === 200) {
         const { code, msg } = data;
         if (code === 0) {
-          message.success("successfully send 1 Ethos to " + to, 30);
+          showMessageWithLink(to, data.data.hash);
         } else {
           err = msg;
           message.error(msg);
         }
       } else {
         err = statusText;
-        message.error("a error happen:" + statusText);
+        message.error('a error happen:' + statusText);
       }
-      console.log(data);
     } catch (error) {
-      err = typeof error == "string" ? error : "a error happen";
-      message.error("a error happen, please try again later");
+      err = typeof error == 'string' ? error : 'a error happen';
+      message.error('a error happen, please try again later');
       console.log(error);
     }
     this.setState({ loading: false, to, hash, err, result: true });
@@ -107,26 +90,44 @@ export default class App extends Component {
       <div className="app">
         <Spin tip="In the transaction......" spinning={loading}>
           <div className="header-logo">
-            <Image preview={false} width={72} height={72} src="/images/coin-faucet.png" />
-            <div style={{ float: "right", marginTop: "18px" }}>
-              <a style={{ fontSize: "20px", fontFamily: "Microsoft YaHei" }} rel="noreferrer" href="http://scan.lucq.fun/" target="_blank">
+            <Image preview={false} width={72} height={72} src="/images/coin-faucet.png" alt="Ethos fuacet" />
+            <div style={{ float: 'right', marginTop: '18px' }}>
+              <a
+                style={{ fontSize: '20px', fontFamily: 'Microsoft YaHei' }}
+                rel="noreferrer"
+                href="https://scan.ethos.cool/"
+                target="_blank">
                 Scan&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
               </a>
-              <a style={{ fontSize: "20px", fontFamily: "Microsoft YaHei" }} rel="noreferrer" href="http://eth.lucq.fun/" target="_blank">
-                Tool&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <a
+                style={{ fontSize: '20px', fontFamily: 'Microsoft YaHei' }}
+                rel="noreferrer"
+                href="https://bridge.ethos.cool/"
+                target="_blank">
+                Bridge&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
               </a>
-              <a style={{ fontSize: "20px", fontFamily: "Microsoft YaHei" }} rel="noreferrer" href="http://uniswap.lucq.fun" target="_blank">
+              <a
+                style={{ fontSize: '20px', fontFamily: 'Microsoft YaHei' }}
+                rel="noreferrer"
+                href="https://swap.ethos.cool"
+                target="_blank">
                 Uniswap&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
               </a>
             </div>
           </div>
           <Row type="flex" justify="center" align="middle" className="content">
-            <Col style={{ minWidth: "500px", maxWidth: "500px" }}>
-              <Card title="PRIVATE CHAIN FAUCET" bordered={true}>
-                <Input ref={(c) => (this.inputAddress = c)} size="large" placeholder="Input your address" allowClear style={{ marginBottom: "15px" }} />
-                <div style={{ margin: "12px 0px" }}>
+            <Col style={{ minWidth: '500px', maxWidth: '500px' }}>
+              <Card title="ETHOS CHAIN FAUCET" bordered={true}>
+                <Input
+                  ref={(c) => (this.inputAddress = c)}
+                  size="large"
+                  placeholder="Input your address"
+                  allowClear
+                  style={{ marginBottom: '15px' }}
+                />
+                <div style={{ margin: '12px 0px' }}>
                   <div onClick={this.send} className="send">
-                    Request 1 Ethos
+                    Request 1 ETHOS
                   </div>
                 </div>
               </Card>
